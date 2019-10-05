a80e81f630fd74e57a86b18d537e84b7e9ac8d23
mhamouda1 <55758648+mhamouda1@users.noreply.github.com>
2019-10-05 10:33:20 -0700

    Update agent_setup.sh

--- agent_setup.sh
+++ agent_setup.sh
@@ -1,8 +1,5 @@
 yum install git java -y
 
-#agent home directory
-mkdir -p /home/jenkins/agent
-
 #move the centos user (or any other user) off id 1000
 usermod centos -u 1005
 groupmod centos -g 1005
@@ -13,6 +10,9 @@ useradd jenkins -g 1000 -u 1000
 
 #add jenkins to sudoers file
 echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+#agent home directory
+mkdir -p /home/jenkins/agent
 
 #permissions
 chown -R 1000:1000 /home/jenkins
